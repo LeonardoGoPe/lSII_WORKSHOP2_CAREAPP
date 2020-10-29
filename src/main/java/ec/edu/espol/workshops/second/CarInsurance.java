01eac71360b144d9088b17471a52e0c60f839cce
Jordy Medina <medinajordy@hotmail.com>
2020-10-28 22:29:09 -0500

    Inicial

---
+++ src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
@@ -0,0 +1,11 @@
+package ec.edu.espol.workshops.second;
+
+public class CarInsurance {
+	
+	public static int  BASE = 500;
+	private CarInsurance() {}
+	
+	public static int calcularPremium(Customer c) {
+		return 1;
+	}
+}
