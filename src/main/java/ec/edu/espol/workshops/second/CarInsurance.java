c67cd9dfbe8f6b4f2909ce0bbbfaa144d7404f14
LeonardoGoPe <leonardo.gomez.1297@gmail.com>
2020-11-08 14:23:40 -0500

    Test empirico, correcciones realizadas en el código.

--- src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
+++ src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
@@ -7,10 +7,18 @@ public final class CarInsurance {
 	
 	public static int calcularPremium(Customer c) {
 		int total= BASE;
-		if(c.validar()==false ) return -1;
-		if(c.edad<65 && c.edad> 45) total-=100;
-		if(c.sexo.equals("M") && c.casado==false && c.edad<25 ) total+=1500;
-		else if(c.sexo.equals("F") && c.casado==true) total-=200;
+		if(c.validar()==false ){ 
+			return -1;
+		}
+		if(c.edad<65 && c.edad> 45) {
+			total-=100;
+		}
+		if(c.sexo.equals("M") && c.casado==false && c.edad<25 ) {
+			total+=1500;
+		}
+		if(c.sexo.equals("F") || c.casado==true) {
+			total-=200;
+		}
 		
 		return total;
 	}
--- src/main/java/ec/edu/espol/workshops/second/main.java
+++ src/main/java/ec/edu/espol/workshops/second/main.java
@@ -2,13 +2,22 @@ package ec.edu.espol.workshops.second;
 
 public class main {
 	public static void main(String args[]) {
-		Customer cliente1 = new Customer(20,"M","201598762",true);
-		System.out.println(CarInsurance.calcularPremium(cliente1)); //Si vale
+		Customer cliente1 = new Customer(85,"M","201598762",true);
+		System.out.println("Cliente 1: "+CarInsurance.calcularPremium(cliente1)); 
 		
-		Customer cliente2 = new Customer(50,"M","201598762",true);
-		System.out.println(CarInsurance.calcularPremium(cliente2)); //Si vale
+		Customer cliente2 = new Customer(50,"F","201598762",true);
+		System.out.println("Cliente 2: "+CarInsurance.calcularPremium(cliente2));
 		
-		Customer cliente3 = new Customer(85,"M","201598762",true);
-		System.out.println(CarInsurance.calcularPremium(cliente3)); //No vale
+		Customer cliente3 = new Customer(19,"M","201598762",false);
+		System.out.println("Cliente 3: "+CarInsurance.calcularPremium(cliente3));
+		
+		Customer cliente4 = new Customer(40,"M","20158762",false);
+		System.out.println("Cliente 4: "+CarInsurance.calcularPremium(cliente4));
+		
+		Customer cliente5 = new Customer(50,"S","201598762",true);
+		System.out.println("Cliente 5: "+CarInsurance.calcularPremium(cliente5));
+		
+		Customer cliente6 = new Customer(16,"F","201598762",false);
+		System.out.println("Cliente 6: "+CarInsurance.calcularPremium(cliente6));
 	}
 }
