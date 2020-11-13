9eb9f59cf7abc352c88c852dacaf6bf78a3c61a9
tiomaldy <itsmemaldy@gmail.com>
2020-11-13 14:04:56 -0500

    Taller nuevo

--- src/main/java/ec/edu/espol/workshops/second/main.java
+++ src/main/java/ec/edu/espol/workshops/second/main.java
@@ -5,7 +5,7 @@ public class main {
 		Customer cliente1 = new Customer(20,"M","201598762",true);
 		System.out.println(CarInsurance.calcularPremium(cliente1)); //Si vale
 		
-		Customer cliente2 = new Customer(50,"M","201598762",true);
+		Customer cliente2 = new Customer(50,"F","201598762",false);
 		System.out.println(CarInsurance.calcularPremium(cliente2)); //Si vale
 		
 		Customer cliente3 = new Customer(85,"M","201598762",true);
