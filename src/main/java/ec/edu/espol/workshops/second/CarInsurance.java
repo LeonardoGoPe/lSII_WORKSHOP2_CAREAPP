5e8c62a7930fb8325cd21b3f77a63b058ddc5980
tiomaldy <itsmemaldy@gmail.com>
2020-12-11 13:47:40 -0500

    Final funciona

--- src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
+++ src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
@@ -2,7 +2,7 @@ package ec.edu.espol.workshops.second;
 
 public final class CarInsurance {
 	
-	public static final int  BASE = 600; //cambio 6
+	public static final int  BASE = 600; //cambio 7
 	private CarInsurance() {}
 	
 	public static int calcularPremium(Customer c) {
